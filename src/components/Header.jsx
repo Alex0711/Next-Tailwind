af11f54c5e371dd67feeed08cdead9a809615c45
Alex0711 <alex.senger@hotmail.com>
2022-08-09 22:02:11 -0300

    fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,8 @@ export default function Header() {
     email: auth?.user?.email,
     imageUrl: `https://ui-avatars.com/api/?name=${auth?.user?.name}`,
   };
-  console.log(userData);
+  console.log({ userData });
+
   return (
     <>
       <Disclosure as="nav" className="bg-gray-800">
@@ -129,11 +130,11 @@ export default function Header() {
               <div className="pt-4 pb-3 border-t border-gray-700">
                 <div className="flex items-center px-5">
                   <div className="flex-shrink-0">
-                    <img className="h-10 w-10 rounded-full" src={userData.imageUrl} alt="" />
+                    <img className="h-10 w-10 rounded-full" src={`https://ui-avatars.com/api/?name=${auth?.user?.name}`} alt="" />
                   </div>
                   <div className="ml-3">
-                    <div className="text-base font-medium leading-none text-white">{userData.name}</div>
-                    <div className="text-sm font-medium leading-none text-gray-400">{userData.email}</div>
+                    <div className="text-base font-medium leading-none text-white">{auth?.user?.name}</div>
+                    <div className="text-sm font-medium leading-none text-gray-400">{auth?.user?.email}</div>
                   </div>
                   <button
                     type="button"
